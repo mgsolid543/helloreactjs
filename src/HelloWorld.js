fab5831cf102ed9d21a8714bc33a4588dca5fd3f
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-06 05:02:55 +0700

    state interaktif

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,9 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class HelloWorld extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { salam : 'Hello' };
+    }
+
+    salamkan = () => {
+        this.setState({ salam: 'Hi!'})
+    }
+
+
+
   render() {
     return (
-  <div className="warna">Hello {this.props.namaDepan} {this.props.namaBelakang}</div>
+        <div className="warna">
+            {this.state.salam} {this.props.namaDepan} {this.props.namaBelakang}
+            <br/>
+            <button onClick={this.salamkan}>Say Hi!</button>
+        </div>
     )
   }
 }
