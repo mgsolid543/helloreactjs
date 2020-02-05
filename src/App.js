5eb374494088219945bb88dd1a997cc8d2ccebe8
Irpan Budiana <irpan.budiana@gmail.com>
2020-02-06 04:50:19 +0700

    memahami component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import HelloWorld from './HelloWorld';
 
 class App extends Component {
   render() {
     return (
-      <div className="warna">Hello everybody!</div>
+      <div>
+        <HelloWorld />
+        <HelloWorld />
+        <HelloWorld />
+      </div>
     )
   }
 }
